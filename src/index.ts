440c13f569821a7505d45d4e280694c65ee1f932
ZZhengyu1127 <rory.3508978@gmail.com>
2025-03-19 19:01:14 +0800

    refactor(ts): 變更產品建構式

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
 import { Product } from "./product";
-let product = {
-    id: 1,
-    name: "圖像 Angular 開發入門 第二版",
-    company: "博碩文化",
-    price: 500,
-    isShow: true,
-    createDate: new Date("2024-4-10"),
-    modifyDate: undefined,
-};
-
-new Product(
+let product = new Product(
     1,
     "圖像 Angular 開發入門 第二版",
     "博碩文化",
     500,
     true,
-    new Date("2024-4-10"),
-    undefined,
+    new Date("2024-4-10")
 );
 console.log(product);
 console.table(product);
