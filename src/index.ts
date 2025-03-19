af2b785148049b7dc3459ed19418f083a329c787
ZZhengyu1127 <rory.3508978@gmail.com>
2025-03-19 18:54:07 +0800

    feat(ts): 利用類別定義產品資料

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Product } from "./product";
 let product = {
     id: 1,
     name: "圖像 Angular 開發入門 第二版",
@@ -7,5 +8,15 @@ let product = {
     createDate: new Date("2024-4-10"),
     modifyDate: undefined,
 };
+
+new Product(
+    1,
+    "圖像 Angular 開發入門 第二版",
+    "博碩文化",
+    500,
+    true,
+    new Date("2024-4-10"),
+    undefined,
+);
 console.log(product);
 console.table(product);
